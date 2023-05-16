55979ed4b136e59e98e82b83c7ca4b6526b615d3
Hemant Bhandari <zenithtaiga@gmail.com>
2023-05-16 10:06:54 +0530

    Clicking on icon now navigates to home

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,13 +1,13 @@
 "use client";
 
+import Link from 'next/link';
 import Login from './Login';
 
 export default async function Header() {
     return (
         <header className='flex flex-row'>
             <nav className='navbar bg-base-100'>
-                <a className='btn btn-ghost normal-case text-2xl'><span className='text-primary'>CU</span>talkies</a>
-            </nav>
+                <Link href="/" className='btn btn-ghost normal-case text-2xl'><span className='text-primary'>CU</span>talkies</Link>            </nav>
 
             <nav className='navbar bg-base-100 justify-end pr-6'>
                 <Login />
